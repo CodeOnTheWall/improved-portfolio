33608f69a52d8af56c3e0209b87be9b53a5e4bb5
Bralen Sundquist <bralencsundquist@hotmail.com>
2023-08-08 11:09:32 -0600

    fixed resume file error

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function page() {
             </p>
             <div className=" flex items-center self-start mt-2 lg:self-center">
               <Link
-                href="/BralenFullStackExtraodinaire.docx"
+                href="/BralenFullStackExtraodinaire.pdf"
                 target={"_blank"}
                 className=" flex items-center bg-dark text-light p-2.5 px-6
             rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
